af1e916ed81ad778dfccba189450ce2057fb275f
Emily <emily.zy.lin@gmail.com>
2024-10-16 10:01:48 -0500

    finish Dictionaries

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -2,5 +2,37 @@
 
 //TODO: Write your code for Dictionary Practice below:
 
-Console.WriteLine("Hello, Dictionary!");
+Dictionary<string, int> students = new Dictionary<string, int>();
+
+string studentName;
+
+do
+{
+    Console.WriteLine("Whats your name");
+    string input = Console.ReadLine();
+    studentName = input;
+
+    if (!Equals(studentName, ""))
+    {
+        Console.WriteLine("whats your id");
+        input = Console.ReadLine();
+        int studentId = int.Parse(input);
+        students.Add(studentName, studentId);
+
+    }
+
+}
+while (!Equals(studentName,"")); //condition, while this is true, run the above 
+
+Console.WriteLine("here's a list of all the students");
+
+foreach(KeyValuePair<string, int> student in students)
+{
+    Console.WriteLine(student.Key + " : " + student.Value);
+}
+
+
+
+
+
 
