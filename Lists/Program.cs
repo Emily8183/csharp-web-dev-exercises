e1c6de109cc61fe81f1677dfb0e4a02b7f61488f
Emily <emily.zy.lin@gmail.com>
2024-10-15 20:46:45 -0500

    finish Lists exe

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -2,4 +2,67 @@
 
 //TODO: Write your code for List Practice below:
 
-Console.WriteLine("Hello, Lists!");
+using System.Numerics;
+
+List<int> sample = new List<int>();
+
+sample.Add(1);
+sample.Add(2);
+sample.Add(3);
+sample.Add(4);
+sample.Add(5);
+sample.Add(6);
+sample.Add(7);
+sample.Add(8);
+sample.Add(9);
+sample.Add(10);
+sample.Add(11);
+
+//to call the CalSum function
+int sum = CalSum(sample);
+Console.WriteLine("the sum is " + sum);
+
+Console.WriteLine("Choose a length: ");
+string inputLength = Console.ReadLine();
+int intLength = int.Parse(inputLength);
+
+
+
+List<string> sample2 = new List<string>();
+
+sample2.Add("abcde");
+sample2.Add("abcd2");
+sample2.Add("abd");
+
+//to call the PrintLetters function
+PrintLetters(sample2, intLength);
+
+static int CalSum(List<int> sample)
+{
+    int sum = 0; 
+
+    for (int i = 0; i < sample.Count; i++)
+    {
+        if (sample[i] % 2 == 0)
+        {
+            sum += sample[i];
+        }
+    }
+
+    return sum;
+}
+
+static void PrintLetters(List<string> strings, int chosenLength)
+{
+    foreach (string s in strings)
+    {
+        if (s.Length == chosenLength)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
+
+
+
+
