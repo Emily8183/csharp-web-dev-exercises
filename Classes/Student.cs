3c72285cf097f5b5d7420e6cd4424880966261ce
Emily <emily.zy.lin@gmail.com>
2024-10-16 15:57:07 -0500

    update student in class

--- Classes/Student.cs
+++ Classes/Student.cs
@@ -4,8 +4,24 @@ namespace Classes;
 
 public class Student
 {
-    private string Name;
-    private int StudentId;
-    private int NumberOfCredits;
-    private double Gpa;
+    //Auto-implemented properties
+    private string Name { get; set; }
+    private int StudentId { get; set; }
+    private int NumberOfCredits { get; set; }
+    private double Gpa { get; set; }
+
+    //constructor
+
+    public Student(string name, int studentId, int numberOfCredits, double gpa)
+    {
+        Name = name;
+        StudentId = studentId;
+        NumberOfCredits = numberOfCredits;
+        Gpa = gpa;
+    }
+
+    //overload
+    public Student(string name, int studentId, int numberOfCredits)
+        : this(name, studentId, numberOfCredits, 0) { }
+
 }
